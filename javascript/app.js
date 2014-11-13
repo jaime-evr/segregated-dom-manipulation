7c907d3ff41743987e21c19f74d204f8dc4b892e
Jaime Victoria <jaime.victoria@crowdint.com>
2014-11-12 19:29:15 -0600

    Validate form

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,29 @@
+
 $(document).ready(function() {
   var userFormValidator = new UserFormValidator();
+  var formValidator = new FormValidator();
 
-  var validate = function() {
+  var validate = function(userData) {
+    var messages = [];
+
+    if(formValidator.invalidName(userData.name)) {
+      messages.push('Invalid Name');
+    }
+
+    if(formValidator.invalidEmail(userData.email)) {
+      messages.push('Invalid Email');
+    }
+
+    return messages;
+  };
+
+  var handleValidation = function() {
     var userData = userFormValidator.$getFormFields();
-    console.log(userData);
-  }
+    var messages = validate(userData);
+    userFormValidator.$updateValidationMessages(messages);
+  };
 
-  userFormValidator.$onFormChanged(validate)
+  userFormValidator.$onFormChanged(handleValidation);
 });
+
 
--- javascript/user-form-validator.js
+++ javascript/user-form-validator.js
@@ -1,21 +1,39 @@
+
 var UserFormValidator = function() {
   this.$form = $('#user-info');
-};
+  this.$validationErrors = $('#validation-errors');
 
-//DOM inspection
-UserFormValidator.prototype.$getFormFields = function() {
-  return {
-    name: this.$form.find('#name').val(),
-    email: this.$form.find('#email').val(),
-    password: this.$form.find('#password').val()
+  //DOM inspection
+  this.$getFormFields = function() {
+    return {
+      name: this.$form.find('#name').val(),
+      email: this.$form.find('#email').val()
+    };
+  };
+
+  //DOM event
+  this.$onFormChanged = function(validate) {
+    this.$form.find('input').on('change', validate)
+  };
+
+  //DOM change
+  this.$updateValidationMessages = function(messages) {
+    var validationErrors = $('#validation-errors');
+    validationErrors.empty();
+    messages.forEach(function(message) {
+      validationErrors.append($('<div>').text(message));
+    });
   };
 };
 
-//DOM events
-UserFormValidator.prototype.$onFormChanged = function(validate) {
-  this.$form.find('input').on('change', validate)
-};
+var FormValidator = function() {
+  this.invalidName = function(name) {
+    return !name.lenght > 5
+  };
 
-//var Validator = function() {
-//};
+  this.invalidEmail = function(email) {
+    var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    return !pattern.test(email)
+  };
+};
 
